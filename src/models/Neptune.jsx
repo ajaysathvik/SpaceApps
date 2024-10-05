82b7fbe1d0dc9b2693b4fcfcd8cc085e50d2f9c9
ajay sathvik <ajaysathvikk@gmail.com>
2024-10-05 13:51:50 +0530

    everything good

--- src/models/Neptune.jsx
+++ src/models/Neptune.jsx
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-
-import React, { useRef } from 'react'
-import { useGLTF } from '@react-three/drei'
-const Neptune =(props)=> {
-  const { nodes, materials } = useGLTF('/Neptune.glb')
-  return (
-    <group {...props} dispose={null}>
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.Neptune.geometry}
-        material={materials['Default OBJ.001']}
-        rotation={[Math.PI / 2, 0, 0]}
-      />
-    </group>
-  )
-}
-
-useGLTF.preload('/Neptune.glb');
-=======
 import React, { useRef } from 'react'
 import { useGLTF } from '@react-three/drei'
 import NeptuneModel from '../assets/3d/Neptune.glb'
@@ -37,5 +16,4 @@ const Neptune = (props) =>{
   )
 }
 
->>>>>>> 5ef73d360d9f8b36c328f168502547a820084608
 export default Neptune;
