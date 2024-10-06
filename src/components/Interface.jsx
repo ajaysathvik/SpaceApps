8747ee8291fcf74f46b106b4a88c4e66503fb0aa
Rahul <rahulrajesh@2101.gmail.com>
2024-10-06 09:30:32 +0530

    feat: dynamic location.

--- src/components/Interface.jsx
+++ src/components/Interface.jsx
@@ -1,35 +1,61 @@
 import { TiMediaRecord } from "react-icons/ti";
+
 const Section = (props) => {
   const { children } = props;
   return (
-    <section className="h-screen w-screen  max-w-screen mx-auto pt-32 flex flex-col items-start text-white">
-      {" "}
-      {/* Center items for better layout */}
+    <section className="h-screen w-screen max-w-screen mx-auto pt-32 flex flex-col items-start text-white">
       {children}
     </section>
   );
 };
 
-export const Interface = () => {
-  const myArray = [
-    "Sun",
-    "Mercury",
-    "Venus",
-    "Earth",
-    "Mars",
-    "Jupyter",
-    "Saturn",
-    "Uranus",
-    "Neptune",
+export const Interface = (props) => {
+  const {
+    sunRef,
+    mercuryRef,
+    venusRef,
+    earthRef,
+    marsRef,
+    jupiterRef,
+    saturnRef,
+    uranusRef,
+    neptuneRef,
+  } = props;
+  const handleClick = (ref) => {
+    if (ref.current) {
+      // Set the camera position to the planet's position
+      const position = ref.current.position;
+      camera.position.set(position.x, position.y, position.z + 5); // Adjust camera position as needed
+      camera.lookAt(position); // Make the camera look at the planet
+    }
+  };
+  const planets = [
+    { name: "Sun", ref: sunRef },
+    { name: "Mercury", ref: mercuryRef },
+    { name: "Venus", ref: venusRef },
+    { name: "Earth", ref: earthRef },
+    { name: "Mars", ref: marsRef },
+    { name: "Jupiter", ref: jupiterRef },
+    { name: "Saturn", ref: saturnRef },
+    { name: "Uranus", ref: uranusRef },
+    { name: "Neptune", ref: neptuneRef },
   ];
-  const myList = myArray.map((item) => (
-    <div className="group pl-10 flex flex-col justify-center items-center transition duration-300">
+
+  const myList = planets.map(({ name, ref }) => (
+    <div
+      key={name}
+      className="group pl-10 flex flex-col justify-center items-center transition duration-300"
+      onClick={() => {
+        console.log(ref.current.position);
+      }}
+    >
       <TiMediaRecord className="text-4xl group-hover:text-6xl transition duration-300" />
       <p className="text-lg group-hover:text-2xl transition duration-300">
-        {item}
+        {name}
       </p>
     </div>
   ));
+
   return (
     <>
       <Section>
