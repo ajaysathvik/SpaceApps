a4c83b58887c166145292841559d5ecf94a34833
Rahul <rahulrajesh@2101.gmail.com>
2024-10-06 11:18:48 +0530

    feat: camera

--- src/components/Interface.jsx
+++ src/components/Interface.jsx
@@ -1,15 +1,17 @@
+import { useFrame ,useThree} from '@react-three/fiber';
+import React,{useState} from 'react';
 import { TiMediaRecord } from "react-icons/ti";
+import * as THREE from 'three';
 
-const Section = (props) => {
-  const { children } = props;
-  return (
-    <section className="h-screen w-screen max-w-screen mx-auto pt-32 flex flex-col items-start text-white">
-      {children}
-    </section>
-  );
-};
+
+const Section = ({ children }) => (
+  <section className="h-screen w-screen max-w-screen mx-auto pt-32 flex flex-col items-start text-white">
+    {children}
+  </section>
+);
 
 export const Interface = (props) => {
+  const { camera } = useThree();
   const {
     sunRef,
     mercuryRef,
@@ -21,14 +23,8 @@ export const Interface = (props) => {
     uranusRef,
     neptuneRef,
   } = props;
-  const handleClick = (ref) => {
-    if (ref.current) {
-      // Set the camera position to the planet's position
-      const position = ref.current.position;
-      camera.position.set(position.x, position.y, position.z + 5); // Adjust camera position as needed
-      camera.lookAt(position); // Make the camera look at the planet
-    }
-  };
+ 
+
   const planets = [
     { name: "Sun", ref: sunRef },
     { name: "Mercury", ref: mercuryRef },
@@ -40,17 +36,25 @@ export const Interface = (props) => {
     { name: "Uranus", ref: uranusRef },
     { name: "Neptune", ref: neptuneRef },
   ];
+  const [active, setActive] = useState(planets[0].ref);
+  useFrame(() => {
+    const { x, y, z } = active.current.position;
+
+    
+    camera.position.set(x , y+20 , z-850 ); 
+    // camera.lookAt(active.current.position); 
+  });
+
+  console.log(active);
 
   const myList = planets.map(({ name, ref }) => (
     <div
       key={name}
-      className="group pl-10 flex flex-col justify-center items-center transition duration-300"
-      onClick={() => {
-        console.log(ref.current.position);
-      }}
+      className="group pl-10 flex flex-col justify-center items-center transition duration-300 cursor-pointer"
+      onClick={() => setActive(ref)}
     >
       <TiMediaRecord className="text-4xl group-hover:text-6xl transition duration-300" />
-      <p className="text-lg group-hover:text-2xl transition duration-300">
+      <p className="text-lg group-hover:text-2xl transition duration-300" onClick={()=>{active(ref)}}>
         {name}
       </p>
     </div>
